094386408b74da3196534ecaf623c02c2fe33a63
ergardt <55143723+ergardt@users.noreply.github.com>
2019-12-18 12:23:32 +0200

    Update gas.cpp

--- gas.cpp
+++ gas.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include "gas.h"
+#include <SFML/Window.hpp>
 
-const float dt = 0.1;
+const float dt = 0.05;
 
 
 
@@ -9,27 +10,29 @@ const float dt = 0.1;
 
 int main()
 {
-
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+ 
+    sf::RenderWindow window(sf::VideoMode(800, 600), "gas");
     Ball *Balls = new Ball[100];
     for (int i = 0; i <10; i++)
     {
         for (int j = 0; j < 10; j ++)
         {
-            Balls[i * 10 + j - 1] = Ball(vector2f((i+1)*72, (j+1)*54), vector2f(i ,j), 10, 200, 50, 200);
+            Balls[i * 10 + j] = Ball(Vector2f((i+1)*72, (j+1)*54), Vector2f((i+1)*2 ,(j+1)*3), 10, 200, 50, 200);
         }        
     }
 
 
     while (window.isOpen())
     {
+       
         window.clear();
+
         for (;;)
         {
             for (int k = 0; k < 100; k++ )
             {
-                Balls[k].drawBall(Balls[k], &window);
-                Balls[k].moveBall(&Balls[k]);
+                Balls[k].draw(&window);
+                Balls[k].move(dt);
                 for (int m = 0; m < 100; m++)
                 {
                     collideBalls(&Balls[m], &Balls[k]);
@@ -39,6 +42,7 @@ int main()
         window.display();
     }
     }
-
+    delete [] Balls;
     return 0;
+
 }
