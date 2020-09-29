715a244836de613dbf543c19759956c24a588d15
ergardt <55143723+ergardt@users.noreply.github.com>
2020-09-29 23:33:19 +0300

    Update eqkek.py

--- eqkek.py
+++ eqkek.py
@@ -33,7 +33,28 @@ print(matrix)
 
 A = float(max(abs(a2), abs(a3), abs(a4), abs(a5), abs(a6), abs(a7)))
 B = float(max(abs(a2), abs(a3), abs(a4), abs(a5), abs(a6), abs(a1)))
-minBorder = float(abs(a7) / ( abs(a7) + B))
+minBorder = float(abs(a7) / (abs(a7) + B))
 maxBorder = 1 + A/a1
+center = (maxBorder - minBorder)/2
 
-print("Нижняя граница ",minBorder, "Верхняя граница ", maxBorder)
+#print("Нижняя граница ",minBorder, "Верхняя граница ", maxBorder)
+
+
+# zMatrix = numpy.array([z^(2*n)], [z^(n + 2)], [z^(n + 1)], [z^n], [z^2], [z], [1])
+def equate(b1, b2, b3, b4, b5, b6, b7, y):
+    return b1 * y**(2*n) + b2 * y**(n+2) + b3 * y**(n+1) + b4 * y**n + b5 * y**2 + b6 * y + b7
+
+
+while maxBorder - minBorder > 0.000000001:
+    if equate(a1, a2, a3, a4, a5, a6, a7, minBorder)*equate(a1, a2, a3, a4, a5, a6, a7, center) < 0:
+        maxBorder = center
+        print('ok')
+    else:
+        if equate(a1, a2, a3, a4, a5, a6, a7, maxBorder)*equate(a1, a2, a3, a4, a5, a6, a7, center) < 0:
+            minBorder = center
+            print('ok2')
+
+else:
+    print("Нижняя граница ", minBorder, "Верхняя граница ", maxBorder)
+
+
